edb826d5e472568ac510681ba657b140c4d0c1db
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-20 09:52:02 -0600

    cookie session stored

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
 const cookieSession = require('cookie-session');
 const express = require('express');
 const bodyParser = require('body-parser');
- 
+
 const app = express();
 
 app.use(bodyParser.json());
- 
+
 // Session setup
 // Makes req.session a thing
-app.use(cookieSession({
-  name: 'session',
-  keys: ['session'],
- 
-  // Cookie Options
-  maxAge: 2 * 60 * 1000 // 2 minutes
-}));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['session'],
 
-app.post('/add-click', (req,res) => {
+    // Cookie Options
+    maxAge: 2 * 60 * 1000, // 2 minutes
+  })
+);
+
+app.post('/add-click', (req, res) => {
   // This is defensive code
   // It is checking req.session.totalClicks to see if it exists. If so, use that number. If not, start at 0
   req.session.totalClicks = req.session.totalClicks || 0;
@@ -25,9 +27,26 @@ app.post('/add-click', (req,res) => {
 });
 
 app.get('/get-clicks', (req, res) => {
-  req.session.totalClicks = req.session && req.session.totalClicks || 0;
-  const {totalClicks} = req.session;
-  res.send({totalClicks});
+  req.session.totalClicks = (req.session && req.session.totalClicks) || 0;
+  const { totalClicks } = req.session;
+  res.send({ totalClicks });
+});
+
+app.post('/add-username', (req, res) => {
+  const userData = req.body;
+  console.log('post:', userData);
+  req.session.username = userData.username;
+  res.sendStatus(200);
+});
+
+app.get('/get-username', (req, res) => {
+  const user = req.session.username;
+  console.log('get:', user);
+  res.send({ username: user });
+  // req.session.username =
+  //   (req.session && req.session.username) || 'nothing getting';
+  // const { username } = req.session;
+  // res.send({ username });
 });
 
 // App Set //
--- src/App.js
+++ src/App.js
@@ -5,50 +5,81 @@ class App extends Component {
   state = {
     clickCount: 0,
     username: '',
-    usernameIsEditable: false
-  }
+    usernameIsEditable: false,
+  };
 
   componentDidMount() {
     this.getCount();
+    this.getUsername();
   }
-
 
   // Adding a click means adding to the number that lives on the server
   // We'll POST here
   handleClick = () => {
-    axios.post('/add-click')
+    axios
+      .post('/add-click')
       .then(() => this.getCount())
-      .catch(error => {
+      .catch((error) => {
         console.log('error making add click post', error);
       });
-  }
+  };
 
-  
-  // Our actual count lives on the server in a session. 
+  saveUsername = (event) => {
+    const username = this.state.username;
+    this.postUsername(username);
+    this.setState({
+      usernameIsEditable: false,
+    });
+  };
+
+  postUsername = (user) => {
+    axios
+      .post('/add-username', { username: user })
+      .then(() => this.getUsername())
+      .catch((error) => {
+        console.log('error in name post');
+      });
+  };
+
+  getUsername = () => {
+    axios
+      .get('/get-username')
+      .then((response) => {
+        this.setState({
+          username: response.data.username,
+        });
+      })
+      .catch((error) => {
+        console.log('error adding username', error);
+      });
+  };
+
+  // Our actual count lives on the server in a session.
   // We need to GET that count
   getCount = () => {
-    axios.get('/get-clicks')
-      .then(response => {
+    axios
+      .get('/get-clicks')
+      .then((response) => {
         this.setState({
           clickCount: response.data.totalClicks,
         });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log('error making add click post', error);
       });
-  }
+  };
 
   editUsername = () => {
     this.setState({
       usernameIsEditable: true,
     });
-  }
+  };
 
-  saveUsername = () => {
+  handleNameChange = (event) => {
     this.setState({
-      usernameIsEditable: false,
+      username: event.target.value,
     });
-  }
+  };
 
   render() {
     return (
@@ -56,20 +87,24 @@ class App extends Component {
         <center>
           <h1>Click the Cookie!!</h1>
           <p>
-            {/* Username should go here */}
+            {this.state.username}
             {/* The next block of code is conditional rendering.
             Look at the documentation https://reactjs.org/docs/conditional-rendering.html
             if this is new to you. */}
-            {this.state.usernameIsEditable ?
-              <button onClick={this.saveUsername}>Save Username</button> :
+            {this.state.usernameIsEditable ? (
+              <span>
+                <input placeholder="feed me" onChange={this.handleNameChange} />
+                <button onClick={this.saveUsername}>Save Username</button>{' '}
+              </span>
+            ) : (
               <button onClick={this.editUsername}>Edit Username</button>
-            }
+            )}
           </p>
           <p>{this.state.clickCount}</p>
           <span
             role="img"
             aria-label="cookie"
-            style={{fontSize: '100px', cursor: 'pointer'}}
+            style={{ fontSize: '100px', cursor: 'pointer' }}
             onClick={this.handleClick}
           >
             🍪
